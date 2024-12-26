b00bab614ee7114be52c0b3609375211f963fb81
LamNV290304 <nguyenvietlam290304@gmail.com>
2024-12-26 17:11:45 +0700

    Update

---
+++ Project_Swagger/Controllers/AuthController.cs
@@ -0,0 +1,81 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Project_Swagger.DTO;
+using Project_Swagger.Services;
+
+namespace Project_Swagger.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AuthController : ControllerBase
+    {
+        private readonly AuthService _authService;
+        private readonly UserService _userService;
+        private readonly EmailService _emailService;
+
+        public AuthController(AuthService authService, UserService userService, EmailService emailService)
+        {
+            _authService = authService;
+            _userService = userService;
+            _emailService = emailService;
+        }
+
+        [HttpPost("Login")]
+        [AllowAnonymous]
+        public IActionResult Login([FromBody] UserDTO userLogin)
+        {
+            var user = _authService.Authenticate(userLogin);
+
+            if (user != null)
+            {
+                var token = _authService.GenerateToken(user);
+                return Ok(token);
+            }
+
+            return NotFound("User not exist or not verify");
+        }
+
+        [HttpPost("Register")]
+        [AllowAnonymous]
+        public IActionResult Register([FromBody] UserRegisterDTO userRegister)
+        {
+            if (!_authService.IsValidPassword(userRegister))
+            {
+                return UnprocessableEntity();
+            }
+
+            if (userRegister != null)
+            {
+                bool action = _userService.AddAnUser(userRegister);
+                if (action == false)
+                {
+                    return Conflict();
+                }
+            }
+            
+            var verificationCode = userRegister.OTP;
+            var user = _userService.GetUserByEmail(userRegister.Email);
+            if (user != null)
+            {
+
+                var verificationLink = Url.Action("ConfirmEmail", "Account", new { userId = user.UserId, token = verificationCode }, Request.Scheme);
+
+                _emailService.SendEmailAsync(user.Email, "Your code", verificationCode);
+            }
+            return Ok(new { Message = "Registration successful. Please check your email for the verification link." });
+        }
+
+        [HttpPost("Verify")]
+        [AllowAnonymous]
+        public IActionResult Verify([FromBody] string otp)
+        {
+            var user = _userService.GetUserByEmail(otp);
+            if (user != null)
+            {
+                bool result = _userService.UpdateEmailVerified(user.UserId);
+                return Ok(result);
+            }
+            return BadRequest();
+        }
+    }
+}
---
+++ Project_Swagger/Controllers/UserController.cs
@@ -0,0 +1,82 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using System.Security.Claims;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Project_Swagger.Models;
+using Project_Swagger.Services;
+using Microsoft.IdentityModel.Tokens;
+
+namespace Project_Swagger.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UserController : ControllerBase
+    {
+        private readonly UserService _userService;
+
+        public UserController(UserService userService)
+        {
+            _userService = userService;
+        }
+
+        // GET: api/<UserController>
+        [HttpGet("Admin")]
+        [Authorize(Roles = "admin")]
+        public async Task<ActionResult<IEnumerable<User>>> AdminGetInf()
+        {
+            var _user = User?.FindFirstValue(ClaimTypes.NameIdentifier);
+
+            if (_user.IsNullOrEmpty())
+            {
+                return Unauthorized("User not authenticated.");
+            }
+
+            // Giả sử bạn có phương thức này trong service để lấy thông tin người dùng theo tên người dùng
+            if (_user == null)
+            {
+                return NotFound("User not found.");
+            }
+
+            return Ok(new
+            {
+                name = User?.FindFirstValue(ClaimTypes.NameIdentifier),
+                role = User?.FindFirstValue(ClaimTypes.Role),
+                email = User?.FindFirstValue(ClaimTypes.Email)
+            });
+        }
+
+        [HttpGet("User")]
+        [Authorize(Roles = "user")]
+        public async Task<ActionResult<IEnumerable<User>>> UserGetInf()
+        {
+            var _user = User?.FindFirstValue(ClaimTypes.NameIdentifier);
+
+            if (_user.IsNullOrEmpty())
+            {
+                return Unauthorized("User not authenticated.");
+            }
+
+            // Giả sử bạn có phương thức này trong service để lấy thông tin người dùng theo tên người dùng
+            if (_user == null)
+            {
+                return NotFound("User not found.");
+            }
+
+            return Ok(new
+            {
+                name = ClaimTypes.NameIdentifier.ToString(),
+                role = ClaimTypes.Role.ToString(),
+                email = ClaimTypes.Email.ToString()
+            });
+        }
+
+        [HttpGet("Publice")]
+        [AllowAnonymous]
+        public async Task<ActionResult<User>> Public()
+        {
+            var _user = "Must login";
+
+            return Ok(_user);
+        }
+    }
+}
