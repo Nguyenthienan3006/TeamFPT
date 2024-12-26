284b721da90a127ff10d1458344551b6e59a1d4a
LamNV290304 <nguyenvietlam290304@gmail.com>
2024-12-26 17:12:03 +0700

    update model user

--- Project_Swagger/Models/User.cs
+++ Project_Swagger/Models/User.cs
@@ -2,11 +2,14 @@
 {
     public class User
     {
-        public int UserId { get; set; }       // user_id trong bảng
-        public string Username { get; set; } // username trong bảng
-        public string Password { get; set; } // password trong bảng
-        public string Email { get; set; }    // email trong bảng
-        public string Role { get; set; } // created_at trong bảng
+        public int UserId { get; set; }      
+        public string Username { get; set; } 
+        public string Password { get; set; } 
+        public string Fullname { get; set; }
+        public string Email { get; set; }    
+        public string Role { get; set; }
+        public string OTP { get; }
+        public bool IsEmailVerified { get; set; }
 
     }
 }
