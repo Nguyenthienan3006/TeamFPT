cb471c308f430144d19837be65d2e769eff2d5b3
LamNV290304 <nguyenvietlam290304@gmail.com>
2024-12-26 17:10:39 +0700

    Create UserRegisterDTO

--- Project_Swagger/DTO/UserRegisterDTO.cs
+++ Project_Swagger/DTO/UserRegisterDTO.cs
@@ -3,10 +3,12 @@
     public class UserRegisterDTO
     {
         public string UserName { get; set; }
+        public string Fullname { get; set; }
         public string Password { get; set; }
         public string Repassword { get; set; }
         public string Email { get; set; }
-
-        public string Role = "user";
+        public string Role { get; } = "user";
+        public string OTP { get; } = Guid.NewGuid().ToString();
+        public bool IsEmailVerified { get;} = false;
     }
 }
