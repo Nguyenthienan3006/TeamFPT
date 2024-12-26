3e197d67fe8070e51047269a5829f9381b64aad8
HungHBHE187115 <hunghbhe187115@fpt.edu.vn>
2024-12-26 09:34:30 +0700

    Create UserRepositories.cs

---
+++ TeamFPT/TeamFPT/Repositories/UserRepository.cs
@@ -0,0 +1,12 @@
+﻿namespace TeamFPT.Repositories
+{
+    public class UserRepository
+    {
+        private readonly string _connectionString;
+
+        public UserRepository(IConfiguration configuration)
+        {
+            _connectionString = configuration.GetConnectionString("DefaultConnection");
+        }
+    }
+}
