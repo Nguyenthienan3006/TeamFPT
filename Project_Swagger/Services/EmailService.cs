42452dff7c607a8b945c189510fd7d371fcae785
LamNV290304 <nguyenvietlam290304@gmail.com>
2024-12-26 17:13:02 +0700

    Email Service

---
+++ Project_Swagger/Services/EmailService.cs
@@ -0,0 +1,47 @@
+﻿using System.Net.Mail;
+using System.Net;
+
+namespace Project_Swagger.Services
+{
+    public class EmailService
+    {
+        private readonly string _smtpServer;
+        private readonly int _smtpPort;
+        private readonly string _smtpUser;
+        private readonly string _smtpPassword;
+
+        public EmailService(string smtpServer, int smtpPort, string smtpUser, string smtpPassword)
+        {
+            _smtpServer = smtpServer;
+            _smtpPort = smtpPort;
+            _smtpUser = smtpUser;
+            _smtpPassword = smtpPassword;
+        }
+
+        public async Task SendEmailAsync(string toEmail, string subject, string body)
+        {
+            try
+            {
+                var mailMessage = new MailMessage
+                {
+                    From = new MailAddress(_smtpUser),
+                    Subject = subject,
+                    Body = body,
+                    IsBodyHtml = true
+                };
+                mailMessage.To.Add(toEmail);
+                using (var smtpClient = new SmtpClient(_smtpServer, _smtpPort))
+                {
+                    smtpClient.Credentials = new NetworkCredential(_smtpUser, _smtpPassword);
+                    smtpClient.EnableSsl = true; 
+                    await smtpClient.SendMailAsync(mailMessage);
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Failed to send email: {ex.Message}");
+                throw;
+            }
+        }
+    }
+}
