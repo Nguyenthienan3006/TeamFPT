5931fb602abae2484b8b36742c42c6acc5947422
LamNV290304 <nguyenvietlam290304@gmail.com>
2024-12-27 11:22:24 +0700

    Add Api

--- Project_Swagger/Controllers/UserController.cs
+++ Project_Swagger/Controllers/UserController.cs
@@ -5,6 +5,7 @@ using Microsoft.AspNetCore.Mvc;
 using Project_Swagger.Models;
 using Project_Swagger.Services;
 using Microsoft.IdentityModel.Tokens;
+using Project_Swagger.DTO;
 
 namespace Project_Swagger.Controllers
 {
@@ -13,11 +14,63 @@ namespace Project_Swagger.Controllers
     public class UserController : ControllerBase
     {
         private readonly UserService _userService;
+        private readonly EmailService _emailService;
 
-        public UserController(UserService userService)
+        public UserController(UserService userService, EmailService emailService)
         {
             _userService = userService;
+            _emailService = emailService;
         }
+
+        [HttpGet("ChangePassword")]
+        [Authorize]
+        public IActionResult ChangePassword()
+        {
+            string email = User?.FindFirstValue(ClaimTypes.Email);
+            if (email == null)
+            {
+                return BadRequest("Email not exist");
+            }
+
+            var verificationCode = Guid.NewGuid().ToString();
+            var user = _userService.GetUserByEmail(email);
+            bool result = _userService.UpdateUserOTP(user.UserId, verificationCode);
+
+            if (!result)
+            {
+                return BadRequest();
+            }
+
+            if (user != null)
+            {
+
+                var verificationLink = Url.Action("ConfirmEmail", "Account", new { userId = user.UserId, token = verificationCode }, Request.Scheme);
+
+                _emailService.SendEmailAsync(user.Email, "Your code", verificationCode);
+            }
+            return Ok("Please check your email for the verification code.");
+        }
+
+        [HttpPost("InputPassword")]
+        [Authorize]
+        public IActionResult InputPassword([FromBody] UserChangerPasswordDTO userChangerPasswordDTO)
+        {
+            if (!_userService.IsValidChangePassword(userChangerPasswordDTO))
+            {
+                return BadRequest("Password not valid");
+            }
+
+            string email = User?.FindFirstValue(ClaimTypes.Email);
+            bool result = _userService.UpdatePassword(userChangerPasswordDTO, email);
+
+            if (!result)
+            {
+                return BadRequest("Unsuccessfully");
+            }
+            return Ok("Password change page");
+        }
+
+
 
         // GET: api/<UserController>
         [HttpGet("Admin")]
@@ -31,7 +84,6 @@ namespace Project_Swagger.Controllers
                 return Unauthorized("User not authenticated.");
             }
 
-            // Giả sử bạn có phương thức này trong service để lấy thông tin người dùng theo tên người dùng
             if (_user == null)
             {
                 return NotFound("User not found.");
@@ -56,7 +108,6 @@ namespace Project_Swagger.Controllers
                 return Unauthorized("User not authenticated.");
             }
 
-            // Giả sử bạn có phương thức này trong service để lấy thông tin người dùng theo tên người dùng
             if (_user == null)
             {
                 return NotFound("User not found.");
