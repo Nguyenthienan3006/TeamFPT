684a000c7708a151d1d0250b5ad02d51f9c329ae
LamNV290304 <nguyenvietlam290304@gmail.com>
2024-12-27 11:23:01 +0700

    Add dto

---
+++ Project_Swagger/DTO/UserChangerPasswordDTO.cs
@@ -0,0 +1,9 @@
+﻿namespace Project_Swagger.DTO
+{
+    public class UserChangerPasswordDTO
+    {
+        public string Password { get; set; }
+        public string Repassword { get; set; }
+        public string OTP { get; set; }
+    }
+}
