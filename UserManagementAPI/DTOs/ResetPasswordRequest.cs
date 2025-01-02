5ab5f76909735a12cac0171820c0effe00db6fd7
Le Viet Loi <leloi091998@gmail.com>
2025-01-02 14:56:59 +0700

    validate

--- UserManagementAPI/DTOs/ResetPasswordRequest.cs
+++ UserManagementAPI/DTOs/ResetPasswordRequest.cs
@@ -1,9 +1,13 @@
-﻿namespace UserManagementAPI.DTOs
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace UserManagementAPI.DTOs
 {
     public class ResetPasswordRequest
     {
+        [Required(ErrorMessage = "Username is not blank")]
         public string Username { get; set; }
         public string Otp { get; set; }
+        [Required(ErrorMessage = "Password is not blank")]
         public string NewPassword { get; set; }
     }
 }
